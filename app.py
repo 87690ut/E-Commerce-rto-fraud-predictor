2079ebfd24e1d052e5c7f804350d104237b1ed6e
Uttam <uttam9695.com@gmail.com>
2026-07-02 20:28:43 +0530

    Database connection with python flask web form for data capturing predicting result and save it in database.(finall update pushed).

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
 from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
 import joblib
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Uttam%239695@localhost:3306/rto_database'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class OrderData(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    mobile_no = db.Column(db.String(15), nullable= False)
+    is_verified = db.Column(db.Boolean, nullable= False)
+    payment_type = db.Column(db.String(50), nullable = False)
+    return_count = db.Column(db.Integer, nullable= False)
+    order_value = db.Column(db.Integer, nullable = False)
+    city_tier = db.Column(db.String(50), nullable = False)
+    prediction_result = db.Column(db.String(50), nullable = False)
+
+with app.app_context():
+    db.create_all()
+
+
 
 # Loading Translater and Model
 
@@ -22,6 +43,7 @@ def predict():
         return_count = int(request.form['Return_Count'])
         is_verified_str = request.form['Is_Verified_Mobile']
         is_verified  = True if is_verified_str == 'True' else False
+        mobile_num = request.form['mobile_no']
 
         input_data = pd.DataFrame([[order_value, city_tier, payment_type, return_count, is_verified]],
                                     columns=['Order_Value', 'City_Tier', 'Payment_Type', 'Return_Count', 'Is_Verified_Mobile'])
@@ -33,6 +55,18 @@ def predict():
             result_text = "🚨Alert! High Risk RTO/Fraud Detected"
         else:
             result_text = "✅Safe Order. No RTO/Fraud Detected"
+
+        new_order = OrderData(
+            mobile_no = mobile_num,
+            is_verified = is_verified,
+            payment_type=payment_type,
+            return_count=return_count,
+            order_value=order_value,
+            city_tier=city_tier,
+            prediction_result=result_text
+        ) 
+        db.session.add(new_order)
+        db.session.commit()
 
     return jsonify({'prediction_text': result_text})
 if __name__ == '__main__':
