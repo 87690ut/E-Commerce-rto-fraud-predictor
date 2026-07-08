4d121a6df6943b122f48d15c6e5610060762277d
Uttam <uttam9695.com@gmail.com>
2026-07-08 10:01:15 +0530

    bug fixing in python fixing port issue

--- app.py
+++ app.py
@@ -156,4 +156,5 @@ def predict():
 
     return jsonify({'prediction_text': result_text})
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host = '0.0.0.0', port = port)
